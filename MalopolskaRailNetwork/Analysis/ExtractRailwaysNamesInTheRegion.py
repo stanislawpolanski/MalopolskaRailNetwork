81373ec1dae37c4aaa43a2f3ada5503f74878869
Staszek <stanislaw.polanski@hotmail.com>
2018-12-20 22:47:35 +0100

    returnNamesAndNumbersOfTheRailwaysInTheRegionDict()

--- MalopolskaRailNetwork/Analysis/ExtractRailwaysNamesInTheRegion.py
+++ MalopolskaRailNetwork/Analysis/ExtractRailwaysNamesInTheRegion.py
@@ -9,3 +9,6 @@ railwaysInTheRegionNamesDict = dict()
 for name in stationsInTheRegion:
     for location in stationsInTheRegion[name]['LocationPoints']:
         railwaysInTheRegionNamesDict[location['NUMER_LINII']] = location['NAZWA_LINII']
+
+def returnNamesAndNumbersOfTheRailwaysInTheRegionDict():
+    return railwaysInTheRegionNamesDict
