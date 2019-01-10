c18b13ab5b41fb0c19584fb28664f54d9533ed32
Staszek <stanislaw.polanski@hotmail.com>
2019-01-10 22:09:48 +0100

    intersectionr of railways wms and db in progress - done loops, no data processing though

--- MalopolskaRailNetwork/Analysis/IntersectWMSWithWWW/IntersectRailwaysFromWMSWithWWW.py
+++ MalopolskaRailNetwork/Analysis/IntersectWMSWithWWW/IntersectRailwaysFromWMSWithWWW.py
@@ -4,11 +4,23 @@ import Analysis.ExtractRailwaysNamesInTheRegion as wmsSource
 wms = wmsSource.returnNamesAndNumbersOfTheRailwaysInTheRegionDict()
 
 #import www railways
-import DataReaders.wwwReaders.wwwReader as wwwSource
+import DataReaders.wwwReaders.wwwReader as dbSource
 
-www = wwwSource.returnRailwayList()
+db = dbSource.returnRailwayList()
 
 #intersect
+
+for wKey in wms:
+    keyExistsInDB = False
+    for dbr in db:
+        match = (dbr['Number'] == wKey)
+        if(match):
+            print('match of railway:', wms[wKey])
+            keyExistsInDB = True
+            break
+    if(not keyExistsInDB):
+        print('no corresponding railway:', wms[wKey])
+    #TODO iteration ok - how to process data
 
 
 #return result
