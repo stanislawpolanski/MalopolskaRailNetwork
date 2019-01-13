d2cadb19211e5638fbeeb486ff58b2e23f5631eb
Staszek <stanislaw.polanski@hotmail.com>
2019-01-13 20:26:43 +0100

    design lacking analysis

---
+++ MalopolskaRailNetwork/Analysis/IntersectWMSWithWWW/__init__.py
@@ -0,0 +1,9 @@
+#read wms
+#read db
+#initialize final rowset
+#helper variable for new id's in final rowset
+#loop through wms
+    #loop through db
+        #find matches
+    #no match - add station
+#return stations
