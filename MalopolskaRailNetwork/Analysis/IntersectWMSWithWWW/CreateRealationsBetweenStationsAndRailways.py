b841537b50d8a74fca5f5909e42300c711f64332
Staszek <stanislaw.polanski@hotmail.com>
2019-01-13 20:36:09 +0100

    design finished

--- MalopolskaRailNetwork/Analysis/IntersectWMSWithWWW/CreateRealationsBetweenStationsAndRailways.py
+++ MalopolskaRailNetwork/Analysis/IntersectWMSWithWWW/CreateRealationsBetweenStationsAndRailways.py
@@ -1,1 +1,17 @@
-#todo design and develop
+#todo develop
+
+#read relations from www
+#read stations (they have railways info) - which file
+import Analysis.IntersectStationsWithUnitsAggregatedAndCSV as stations
+
+#helper variable for new id's
+
+#initialize final relations list
+
+
+#loop through all relations in station file
+    #loop through all relations in db file
+        #if no match then raise a flag
+    #if flag raised then append relation to the final file
+
+#todo return result
