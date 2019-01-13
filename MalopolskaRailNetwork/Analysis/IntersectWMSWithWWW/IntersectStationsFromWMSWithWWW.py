c4e02a75fbae04ccf76fa355c58ad9ef15b60815
Staszek <stanislaw.polanski@hotmail.com>
2019-01-13 20:27:32 +0100

    wrong file - corrected

--- MalopolskaRailNetwork/Analysis/IntersectWMSWithWWW/IntersectStationsFromWMSWithWWW.py
+++ MalopolskaRailNetwork/Analysis/IntersectWMSWithWWW/IntersectStationsFromWMSWithWWW.py
@@ -1,1 +1,10 @@
+#read wms
+#read db
+#initialize final rowset
+#helper variable for new id's in final rowset
+#loop through wms
+    #loop through db
+        #find matches
+    #no match - add station
+#return stations
 
