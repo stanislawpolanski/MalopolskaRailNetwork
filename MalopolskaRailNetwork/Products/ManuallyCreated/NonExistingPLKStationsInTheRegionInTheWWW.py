4821ceea8943676965ed832c320e2601599dea0e
Staszek <stanislaw.polanski@hotmail.com>
2019-01-16 18:29:54 +0100

    manual product

---
+++ MalopolskaRailNetwork/Products/ManuallyCreated/NonExistingPLKStationsInTheRegionInTheWWW.py
@@ -0,0 +1,7 @@
+#this list contains list stations that meets all the condintions:
+# - former PLK
+# - exist in the WWW DB
+# - do not exist in the WMS (destroyed)
+
+nonExistingStations = []
+nonExistingStations.append("Płaza", "Nowy Targ Fabryczny", "Ludźmierz", "Czarny Dunajec", "Rogoźnik Podhalański", "Podczerwone", "Skawce")
