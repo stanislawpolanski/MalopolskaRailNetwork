60041b862805d9854059fbb0b171a232b1024ea3
Staszek <stanislaw.polanski@hotmail.com>
2019-01-22 22:35:10 +0100

    reporting merged railways in progress

--- MalopolskaRailNetwork/Reports/ReportMergedRailways.py
+++ MalopolskaRailNetwork/Reports/ReportMergedRailways.py
@@ -1,2 +1,32 @@
+#import data
+import Analysis.IntersectWMSWithWWW.IntersectRailwaysFromWMSWithWWW_NoLHS as source
+data = source.returnFinalBasicRailwaysRowSet()
+
+#import wms as a hashmap with key = railway number
+import DataReaders.RailWaysFromRailWaysReader as wmsSource
+wmsData = wmsSource.returnListOfRailways()
+
+wmsDict = dict()
+
+for r in wmsData:
+    wmsDict[int(r['NUMER'])] = r
+
+#enrich data with additional info from WMS (like geometries)
+for railway in data:
+    if(int(railway['OwnerId']) in [1, 16] and int(railway['Number']) in wmsDict):
+        railway['Geometry'] = wmsDict[int(railway['Number'])]['GEOM']
+        continue
+    railway['Geometry'] = None
+
+print('done')
+#sort by railway number
+data.sort(key = lambda x: int(x['Number']))
+#return function
+
+#report printing function
+
+#report printing with no geometries
+
+
 #todo develop
 
