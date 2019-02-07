4b6e52b9294a392c6e1c3a3694c53003406a4b3d
Staszek <stanislaw.polanski@hotmail.com>
2019-02-07 21:53:17 +0100

    #19 Reading id's of railway units done

--- MalopolskaRailNetwork/Reports/Datasets/Stations_Dataset.py
+++ MalopolskaRailNetwork/Reports/Datasets/Stations_Dataset.py
@@ -1,8 +1,14 @@
 import Reports.ReportMergedStations as SourceOfStations
 import DataReaders.wwwReaders.wwwReader as SourceOfRelationships
+import Reports.Datasets.RailwayUnits_Dataset as SourceOfRailwaysUnits
 
 stations = SourceOfStations.returnMergedStations()
 relationships = SourceOfRelationships.returnObjectsToRailwaysList()
+railwayUnits = SourceOfRailwaysUnits.ReturnUnitsDataset()
+Hasthable_railwayUnits = dict()
+
+for u in railwayUnits:
+    Hasthable_railwayUnits[u['Name']] = u['Id']
 
 Stations_Dataset = []
 
@@ -28,10 +34,17 @@ stations[OlkuszLhs_index]['WMSData']['LocationPoints'].append(LhsLocation)
 #enrich stations with DB relations, otherwise DB relations = None
 for s in stations:
     Station = dict()
-    Station['Id'] = s['Id']
+    Station['Id'] = int(s['Id'])
     Station['Name'] = s['Name']
-    Station['OwnerId'] = s['OwnerId']
+    Station['OwnerId'] = int(s['OwnerId'])
+    #read railway unit and type of a point
+    if((s['WMSData'] is not None) and ('NAZWA_ZAKLADU' in s['WMSData']) and (s['WMSData']['NAZWA_ZAKLADU'] is not None)):
+        Station['RailwayUnitId'] = Hasthable_railwayUnits[s['WMSData']['NAZWA_ZAKLADU']]
+    else:
+        Station['RailwayUnitId'] = None
+    #read out rest of the wms data
+    #read in db relations
 
-
+print('done so far')
 #TODO
 #process each station and create full relations list
