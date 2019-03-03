efa6e086d5b16ed8e444e7c73ddbe1acb108b97a
Staszek <stanislaw.polanski@hotmail.com>
2019-03-03 18:50:15 +0100

    #31 format changed

--- MalopolskaRailNetwork/Reports/Datasets/Geometries_Dataset.py
+++ MalopolskaRailNetwork/Reports/Datasets/Geometries_Dataset.py
@@ -7,18 +7,23 @@ RailwayUnits_Geometries = Source_RailwayUnits.ReturnGeometriesPartialDataset()
 Stations_Geometries = Source_Stations.ReturnGeometriesPartialDataset()
 
 for r in Railways_Geometries:
-	r['GeometryType'] = 'LineString'
+	r['Value'] = 'LINESTRING(' + r['Value'] + ')'
 
 for u in RailwayUnits_Geometries:
-	u['GeometryType'] = 'MultiPolygon'
+	u['Value'] = 'MULTIPOLYGON(' + u['Value'] + ')'
 
 for s in Stations_Geometries:
-	s['GeometryType'] = 'Point'
+	s['Value'] = 'POINT(' + s['Value'] + ')'
 
 Geometries_Dataset = RailwayUnits_Geometries + Railways_Geometries + Stations_Geometries
+
+for row in Geometries_Dataset:
+	row['Value'] = row['Value'].replace(',', '#')
+	row['Value'] = row['Value'].replace(' ', ',')
+	row['Value'] = row['Value'].replace('#', ' ')
 
 import src.JSONTool as jst
 path = 'C:\\Users\\Dell\\source\\repos\\MalopolskaRailNetwork\\MalopolskaRailNetwork\\Data\\DataSnapshots\\FinalDatasets\\Geometries_Dataset.json'
 
 jst.JSONSerialize(Geometries_Dataset, path)
-print("serialising done")
+print("serialising done - geometries")
